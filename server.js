5f62c61e821f0c1ece1a4ca207689ea96c7b5689
GH-Als <135543276+GH-Als@users.noreply.github.com>
2025-03-13 23:41:25 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html when accessing the root URL
